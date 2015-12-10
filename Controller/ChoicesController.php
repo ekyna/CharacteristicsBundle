c80866c40cc1e54e2b0f7843d0fba6a911be761a
Etienne Dauvergne <contact@ekyna.com>
2015-12-10 14:28:27 +0100

    Fix controller xhr handling (use modal).

--- Controller/ChoicesController.php
+++ Controller/ChoicesController.php
@@ -2,13 +2,16 @@
 
 namespace Ekyna\Bundle\CharacteristicsBundle\Controller;
 
+use Doctrine\DBAL\DBALException;
 use Ekyna\Bundle\CoreBundle\Controller\Controller;
+use Ekyna\Bundle\CoreBundle\Modal\Modal;
 use Ekyna\Component\Characteristics\Entity\ChoiceCharacteristicValue;
 use Ekyna\Component\Characteristics\Form\Type\ChoiceCharacteristicValueType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Validator\Constraints;
 
 /**
  * Class ChoicesController
@@ -20,14 +23,17 @@ class ChoicesController extends Controller
     /**
      * Home action.
      *
-     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function homeAction(Request $request)
+    public function homeAction()
     {
         $this->isGranted('VIEW');
 
-        // TODO admin breadcrumb
+        $this->appendBreadcrumb(
+            'characteristics',
+            'ekyna_characteristics.menu',
+            'ekyna_characteristics_choice_admin_home'
+        );
 
         $schemas = [];
 
@@ -65,6 +71,12 @@ class ChoicesController extends Controller
 
         $definition = $this->getRegistry()->getDefinitionByIdentifier($request->attributes->get('name'));
 
+        $this->appendBreadcrumb(
+            'characteristics',
+            'ekyna_characteristics.menu',
+            'ekyna_characteristics_choice_admin_home'
+        );
+
         $choices = $this->getRepository()->findByDefinition($definition);
 
         return $this->render('EkynaCharacteristicsBundle:Choices:list.html.twig', [
@@ -83,21 +95,30 @@ class ChoicesController extends Controller
     {
         $this->isGranted('CREATE');
 
+        $this->appendBreadcrumb(
+            'characteristics',
+            'ekyna_characteristics.menu',
+            'ekyna_characteristics_choice_admin_home'
+        );
+
+        $isXhr = $request->isXmlHttpRequest();
+        $modal = $this->createModal('new');
+
         $definition = $this->getRegistry()->getDefinitionByIdentifier($request->attributes->get('name'));
 
         $choiceValue = new ChoiceCharacteristicValue();
         $choiceValue->setIdentifier($definition->getIdentifier());
 
-        $form = $this
-            ->createForm(new ChoiceCharacteristicValueType(), $choiceValue, [
-                'admin_mode' => true,
-                '_redirect_enabled' => true,
-                /*'_footer' => array(
-                    'cancel_path' => $this->generateUrl('ekyna_characteristics_choice_admin_list', array('name' => $definition->getIdentifier())),
-                ),*/
-            ])
-            // TODO form_actions
-        ;
+        $form = $this->createChoiceForm(
+            $choiceValue,
+            $this->generateUrl('ekyna_characteristics_choice_admin_new', array(
+                'name' => $definition->getIdentifier(),
+            )),
+            $this->generateUrl('ekyna_characteristics_choice_admin_list', array(
+                'name' => $definition->getIdentifier(),
+            )),
+            $isXhr
+        );
 
         $form->handleRequest($request);
         if ($form->isValid()) {
@@ -106,14 +127,15 @@ class ChoicesController extends Controller
             $em->persist($choiceValue);
             $em->flush();
 
-            if ($request->isXmlHttpRequest()) {
-                return new JsonResponse([
+            if ($isXhr) {
+                $modal->setContent([
                     'id' => $choiceValue->getId(),
                     'name' => $choiceValue->getValue(),
                 ]);
-            } else {
-                $this->addFlash('La resource a été créée avec succès.', 'success');
+                return $this->get('ekyna_core.modal')->render($modal);
             }
+
+            $this->addFlash('La resource a été créée avec succès.', 'success');
 
             if (null !== $redirectPath = $form->get('_redirect')->getData()) {
                 return $this->redirect($redirectPath);
@@ -123,16 +145,19 @@ class ChoicesController extends Controller
                 'name' => $definition->getIdentifier(),
                 'choiceId' => $choiceValue->getId(),
             ]));
-        } elseif ($request->getMethod() === 'POST' && $request->isXmlHttpRequest()) {
-            return new JsonResponse(['error' => $form->getErrors()]);
         }
 
-        $format = 'html';
-        if ($request->isXmlHttpRequest()) {
-            $format = 'xml';
+        if ($isXhr) {
+            $modal
+                ->setContent($form->createView())
+                ->setVars(array(
+                    'form_template' => 'EkynaCharacteristicsBundle:Choices:_form.html.twig',
+                ))
+            ;
+            return $this->get('ekyna_core.modal')->render($modal);
         }
 
-        return $this->render('EkynaCharacteristicsBundle:Choices:new.'.$format.'.twig', [
+        return $this->render('EkynaCharacteristicsBundle:Choices:new.html.twig', [
             'definition' => $definition,
             'form' => $form->createView(),
         ]);
@@ -148,6 +173,12 @@ class ChoicesController extends Controller
     public function showAction(Request $request)
     {
         $this->isGranted('VIEW');
+
+        $this->appendBreadcrumb(
+            'characteristics',
+            'ekyna_characteristics.menu',
+            'ekyna_characteristics_choice_admin_home'
+        );
 
         $definition = $this->getRegistry()->getDefinitionByIdentifier($request->attributes->get('name'));
 
@@ -173,23 +204,34 @@ class ChoicesController extends Controller
     {
         $this->isGranted('EDIT');
 
+        $this->appendBreadcrumb(
+            'characteristics',
+            'ekyna_characteristics.menu',
+            'ekyna_characteristics_choice_admin_home'
+        );
+
+        $isXhr = $request->isXmlHttpRequest();
+        $modal = $this->createModal('edit');
+
         $definition = $this->getRegistry()->getDefinitionByIdentifier($request->attributes->get('name'));
 
+        /** @var ChoiceCharacteristicValue $choiceValue */
         $choiceValue = $this->getRepository()->find($request->attributes->get('choiceId'));
         if(null === $choiceValue) {
             throw new NotFoundHttpException('Characteristic choice not found.');
         }
 
-        $form = $this
-            ->createForm(new ChoiceCharacteristicValueType(), $choiceValue, [
-                'admin_mode' => true,
-                '_redirect_enabled' => true,
-                /*'_footer' => array(
-                    'cancel_path' => $this->generateUrl('ekyna_characteristics_choice_admin_list', array('name' => $definition->getIdentifier())),
-                ),*/
-            ])
-            // TODO form_actions
-        ;
+        $form = $this->createChoiceForm(
+            $choiceValue,
+            $this->generateUrl('ekyna_characteristics_choice_admin_edit', array(
+                'name' => $definition->getIdentifier(),
+                'choiceId' => $choiceValue->getId(),
+            )),
+            $this->generateUrl('ekyna_characteristics_choice_admin_list', array(
+                'name' => $definition->getIdentifier(),
+            )),
+            $isXhr
+        );
 
         $form->handleRequest($request);
         if ($form->isValid()) {
@@ -197,6 +239,14 @@ class ChoicesController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($choiceValue);
             $em->flush();
+
+            if ($isXhr) {
+                $modal->setContent([
+                    'id' => $choiceValue->getId(),
+                    'name' => $choiceValue->getValue(),
+                ]);
+                return $this->get('ekyna_core.modal')->render($modal);
+            }
 
             $this->addFlash('La resource a été modifiée avec succès.', 'success');
 
@@ -208,6 +258,16 @@ class ChoicesController extends Controller
                 'name' => $definition->getIdentifier(),
                 'choiceId' => $choiceValue->getId(),
             ]));
+        }
+
+        if ($isXhr) {
+            $modal
+                ->setContent($form->createView())
+                ->setVars(array(
+                    'form_template' => 'EkynaCharacteristicsBundle:Choices:_form.html.twig',
+                ))
+            ;
+            return $this->get('ekyna_core.modal')->render($modal);
         }
 
         return $this->render('EkynaCharacteristicsBundle:Choices:edit.html.twig', [
@@ -228,6 +288,15 @@ class ChoicesController extends Controller
     {
         $this->isGranted('DELETE');
 
+        $this->appendBreadcrumb(
+            'characteristics',
+            'ekyna_characteristics.menu',
+            'ekyna_characteristics_choice_admin_home'
+        );
+
+        $isXhr = $request->isXmlHttpRequest();
+        $modal = $this->createModal('remove')->setSize(Modal::SIZE_NORMAL);
+
         $definition = $this->getRegistry()->getDefinitionByIdentifier($request->attributes->get('name'));
 
         $choiceValue = $this->getRepository()->find($request->attributes->get('choiceId'));
@@ -237,48 +306,88 @@ class ChoicesController extends Controller
 
         // TODO Warn user about ChoiceCharacteristics associations ?
 
-        $builder = $this->createFormBuilder(null, [
-            'admin_mode' => true,
-            '_redirect_enabled' => true,
-            /*'_footer' => array(
-                'cancel_path' => $this->generateUrl(
-                    'ekyna_characteristics_choice_admin_show',
-                    array(
-                        'name' => $definition->getIdentifier(),
-                        'choiceId' => $choiceValue->getId(),
-                    )
-                ),
-                'buttons' => array(
-                    'submit' => array(
-                        'theme' => 'danger',
-                        'icon'  => 'trash',
-                        'label' => 'ekyna_core.button.remove',
-                    )
-                )
-            ),*/
-        ]);
-
-        $form = $builder
-            ->add('confirm', 'checkbox', [
-                'label' => 'Confirmer la suppression ?',
-                'attr' => ['align_with_widget' => true],
-                'required' => true
+        $form = $this
+            ->createFormBuilder(null, [
+                'action' => $this->generateUrl('ekyna_characteristics_choice_admin_remove', array(
+                    'name' => $definition->getIdentifier(),
+                    'choiceId' => $choiceValue->getId(),
+                )),
+                'attr' => ['class' => 'form-horizontal'],
+                'method' => 'POST',
+                'admin_mode' => true,
+                '_redirect_enabled' => true,
             ])
-            // TODO form_actions
+            ->add('confirm', 'checkbox', [
+                'label' => 'ekyna_core.message.remove_confirm',
+                'attr' => ['align_with_widget' => true],
+                'required' => true,
+                'constraints' => [
+                    new Constraints\IsTrue(),
+                ]
+            ])
             ->getForm()
         ;
 
+        if (!$isXhr) {
+            $cancelPath = $this->generateUrl('ekyna_characteristics_choice_admin_list', array(
+                'name' => $definition->getIdentifier(),
+            ));
+            $form->add('actions', 'form_actions', [
+                'buttons' => [
+                    'remove' => [
+                        'type' => 'submit',
+                        'options' => [
+                            'button_class' => 'danger',
+                            'label' => 'ekyna_core.button.remove',
+                            'attr' => ['icon' => 'trash'],
+                        ],
+                    ],
+                    'cancel' => [
+                        'type' => 'button',
+                        'options' => [
+                            'label' => 'ekyna_core.button.cancel',
+                            'button_class' => 'default',
+                            'as_link' => true,
+                            'attr' => [
+                                'class' => 'form-cancel-btn',
+                                'icon' => 'remove',
+                                'href' => $cancelPath,
+                            ],
+                        ],
+                    ],
+                ],
+            ]);
+        }
+
         $form->handleRequest($request);
         if ($form->isValid()) {
+            $success = false;
             $em = $this->getDoctrine()->getManager();
             $em->remove($choiceValue);
-            $em->flush();
+            try {
+                $em->flush();
+                $success = true;
+            } catch(DBALException $e) {
+                $this->addFlash('ekyna_admin.resource.message.remove.integrity', 'danger');
+            }
 
-            $this->addFlash('La resource a été supprimée avec succès.', 'success');
+            if ($success) {
+                if ($isXhr) {
+                    $modal->setContent(['success' => true]);
+                    return $this->get('ekyna_core.modal')->render($modal);
+                }
 
-            return $this->redirect($this->generateUrl('ekyna_characteristics_choice_admin_list', [
-                'name' => $definition->getIdentifier(),
-            ]));
+                $this->addFlash('La resource a été supprimée avec succès.', 'success');
+
+                return $this->redirect($this->generateUrl('ekyna_characteristics_choice_admin_list', [
+                    'name' => $definition->getIdentifier(),
+                ]));
+            }
+        }
+
+        if ($isXhr) {
+            $modal->setContent($form->createView());
+            return $this->get('ekyna_core.modal')->render($modal);
         }
 
         return $this->render('EkynaCharacteristicsBundle:Choices:remove.html.twig', [
@@ -286,6 +395,106 @@ class ChoicesController extends Controller
             'choice' => $choiceValue,
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * Creates the choice value form.
+     *
+     * @param ChoiceCharacteristicValue $choiceValue
+     * @param string $actionPath
+     * @param string $cancelPath
+     * @param bool|false                $isXhr
+     *
+     * @return \Symfony\Component\Form\Form
+     */
+    private function createChoiceForm(ChoiceCharacteristicValue $choiceValue, $actionPath, $cancelPath, $isXhr = false)
+    {
+        $form = $this
+            ->createForm(new ChoiceCharacteristicValueType(), $choiceValue, [
+                'action' => $actionPath,
+                'method' => 'POST',
+                'attr' => ['class' => 'form-horizontal'],
+                'admin_mode' => true,
+                '_redirect_enabled' => true,
+            ])
+            ->remove('value')
+            ->add('value', 'text', [
+                'label' => 'ekyna_characteristics.choice.field.value',
+                'required' => true,
+            ]);;
+
+        if (!$isXhr) {
+            $form->add('actions', 'form_actions', [
+                'buttons' => [
+                    'save' => [
+                        'type' => 'submit',
+                        'options' => [
+                            'button_class' => 'primary',
+                            'label' => 'ekyna_core.button.save',
+                            'attr' => ['icon' => 'ok'],
+                        ],
+                    ],
+                    'cancel' => [
+                        'type' => 'button',
+                        'options' => [
+                            'label' => 'ekyna_core.button.cancel',
+                            'button_class' => 'default',
+                            'as_link' => true,
+                            'attr' => [
+                                'class' => 'form-cancel-btn',
+                                'icon' => 'remove',
+                                'href' => $cancelPath,
+                            ],
+                        ],
+                    ],
+                ],
+            ]);
+        }
+
+        return $form;
+    }
+
+    /**
+     * Creates a modal object.
+     *
+     * @param string $action
+     * @return Modal
+     */
+    private function createModal($action)
+    {
+        $modal = new Modal(sprintf('ekyna_characteristics.choice.header.%s', $action, $action));
+
+        $buttons = [];
+
+        if (in_array($action, ['new', 'edit', 'remove'])) {
+            $submitButton = [
+                'id'       => 'submit',
+                'label'    => 'ekyna_core.button.save',
+                'icon'     => 'glyphicon glyphicon-ok',
+                'cssClass' => 'btn-success',
+                'autospin' => true,
+            ];
+            if ($action === 'edit') {
+                $submitButton['icon'] = 'glyphicon glyphicon-ok';
+                $submitButton['cssClass'] = 'btn-warning';
+            } elseif ($action === 'remove') {
+                $submitButton['label'] = 'ekyna_core.button.remove';
+                $submitButton['icon'] = 'glyphicon glyphicon-trash';
+                $submitButton['cssClass'] = 'btn-danger';
+            }
+            $buttons[] = $submitButton;
+        }
+
+        $buttons[] = [
+            'id' => 'close',
+            'label' => 'ekyna_core.button.cancel',
+            'icon' => 'glyphicon glyphicon-remove',
+            'cssClass' => 'btn-default',
+        ];
+
+        $modal->setButtons($buttons);
+
+        return $modal;
     }
 
     /**
@@ -343,5 +552,19 @@ class ChoicesController extends Controller
     private function getConfiguration()
     {
         return $this->get('ekyna_characteristics.choice.configuration');
+    }
+
+    /**
+     * Appends a link or span to the admin breadcrumb
+     *
+     * @param string $name
+     * @param string $label
+     * @param string $route
+     *
+     * @param array $parameters
+     */
+    protected function appendBreadcrumb($name, $label, $route = null, array $parameters = [])
+    {
+        $this->container->get('ekyna_admin.menu.builder')->breadcrumbAppend($name, $label, $route, $parameters);
     }
 }
